b97b418f4579dd92c06c9cc2753f21df627d607d
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2023-12-11 23:38:54 +0100

    adding products

---
+++ jsfiles/product.js
@@ -0,0 +1,62 @@
+const products = [
+    {
+        id: "bc2847e9-5323-403f-b7cf-57fde044a955",
+        image: "images/products/men-cozy-fleece-zip-up-hoodie-red.jpg",
+        name: "Men's Full-Zip Hooded Fleece Sweatshirt",
+        rating: {
+            stars: 4.5,
+            count: 3157
+        },
+        priceCents: 2400,
+        keywords: [
+            "sweaters",
+            "hoodies",
+            "apparel",
+            "mens"
+    ]
+    },
+    {
+        id: "aaa65ef3-8d6f-4eb3-bc9b-a6ea49047d8f",
+        image: "images/products/kitchen-paper-towels-30-pack.jpg",
+        name: "2-Ply Kitchen Paper Towels - 30 Pack",
+        rating: {
+        stars: 4.5,
+        count: 1045
+        },
+        priceCents: 5799,
+        keywords: [
+        "kitchen",
+        "kitchen towels",
+        "tissues"
+        ]
+    },
+    {
+        id: "dd82ca78-a18b-4e2a-9250-31e67412f98d",
+        image: "images/products/plain-hooded-fleece-sweatshirt-yellow.jpg",
+        name: "Plain Hooded Fleece Sweatshirt",
+        rating: {
+        stars: 4.5,
+        count: 317
+        },
+        priceCents: 2400,
+        keywords: [
+        "hoodies",
+        "sweaters",
+        "apparel"
+        ]
+    },
+    {
+        id: "d339adf3-e004-4c20-a120-40e8874c66cb",
+        image: "images/products/double-elongated-twist-french-wire-earrings.webp",
+        name: "Double Oval Twist French Wire Earrings - Gold",
+        rating: {
+        stars: 4.5,
+        count: 117
+        },
+        priceCents: 2400,
+        keywords: [
+        "accessories",
+        "womens"
+        ]
+    }
+]
