e84db2318b7740f80e63f699812c6359a668b520
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2023-12-19 00:16:09 +0100

    cart functionalities add to localstorage

--- jsfiles/cart.js
+++ jsfiles/cart.js
@@ -1,4 +1,6 @@
-import {cart} from "./cartItems.js"
+alert("hello")
+
+
 
 const cartButton = document.querySelector(".fa-cart-shopping");
 
@@ -18,40 +20,39 @@ home.addEventListener("click", ()=>{
     window.location.href = "index.html"
 })
 
-console.log("mimi")
 
 
 
 
-let cartHtml = ``
+// let cartHtml = ``
 
-cart.forEach ((cartItem)=>{
-    cartHtml += `
-    <div class="product-contain">
-                <div class="cart-image-container"><img src="${cartItem.image}"></div>
-                <div class="details">
-                    <p>${cartItem.name}</p>
-                    <p class="stock">In stock!!!</p>
-                    <div class="quantity">
-                    <label for="for">Qty</label>
-                    <select name="for">
-                        <option value="1">1</option>
-                        <option value="1">2</option>
-                        <option value="1">3</option>
-                        <option value="1">4</option>
-                        <option value="1">5</option>
-                        <option value="1">6</option>
-                        <option value="1">7</option>
-                        <option value="1">8</option>
-                        <option value="1">9</option>
-                        <option value="1">10</option>
-                    </select>
-                    </div>
-                    <button class="remove">Remove from cart</button>
-                </div>
-                <div><h4>${cartItem.price}</h4></div>
-            </div>
-    `
-})
+// cart.forEach ((cartItem)=>{
+//     cartHtml += `
+//     <div class="product-contain">
+//                 <div class="cart-image-container"><img src="${cartItem.image}"></div>
+//                 <div class="details">
+//                     <p>${cartItem.name}</p>
+//                     <p class="stock">In stock!!!</p>
+//                     <div class="quantity">
+//                     <label for="for">Qty</label>
+//                     <select name="for">
+//                         <option value="1">1</option>
+//                         <option value="1">2</option>
+//                         <option value="1">3</option>
+//                         <option value="1">4</option>
+//                         <option value="1">5</option>
+//                         <option value="1">6</option>
+//                         <option value="1">7</option>
+//                         <option value="1">8</option>
+//                         <option value="1">9</option>
+//                         <option value="1">10</option>
+//                     </select>
+//                     </div>
+//                     <button class="remove">Remove from cart</button>
+//                 </div>
+//                 <div><h4>${cartItem.price}</h4></div>
+//             </div>
+//     `
+// })
 
- document.querySelector(".container1").innerHTML = cartHtml
+// document.querySelector(".container1").innerHTML = cartHtml
--- jsfiles/script.js
+++ jsfiles/script.js
@@ -1,5 +1,21 @@
+// alert("hello")
 import { products } from "./product.js"
-import { cart } from "./cartItems.js"
+// import { cart } from "./cartItems.js"
+
+let cart = [
+    {
+        name: "Trash Can",
+        price: 5000,
+        quantity: 1,
+        image: `trash-can-with-foot-pedal-50-liter.jpg`
+    },
+    {
+        name: "Green Shoe",
+        price: 5090,
+        quantity: 1,
+        image: "men-athletic-shoes-green.jpg"
+    }
+]
 
 let productsHTML = ``
 
@@ -10,7 +26,7 @@ productsHTML += `
         <div class="img-contain"><img src="${product.image}" alt="" class="product-img"></div>
         <h2 class="product-name">${product.name}</h2>
         <div class="add">
-            <p class="price">₦${product.price}</p>
+            <p class="price">₦${product.price}</p> 
             <button class="to-cart js-add" data-product-name="${product.name}">Add to Cart</button>
         </div>
     </div>
@@ -21,7 +37,10 @@ productsHTML += `
 
 })
 
-document.querySelector(".container").innerHTML = productsHTML
+if(window.document.title === "Femi Store"){
+    document.querySelector(".container").innerHTML = productsHTML
+}
+
 
 
 const cartButton = document.querySelector(".fa-cart-shopping");
@@ -31,6 +50,7 @@ cartButton.addEventListener("click", ()=>{
 
     window.location.href = cartUrl
 })
+
 
 
 const home = document.querySelector(".logo")
@@ -44,6 +64,69 @@ const addToCart = document.querySelectorAll(".js-add")
 
 addToCart.forEach((button)=>{
         button.addEventListener ("click", ()=>{
-        console.log(button.dataset.productName)
+        const productName = button.dataset.productName
+        products.forEach((item)=>{
+            if (item.name === productName){
+            cart.push({
+            name: productName,
+            price: item.price,
+            quantity: 1,
+            image: item.image
+        })
+        }
+        localStorage.setItem("carts", JSON.stringify(cart))
+        })
+
+        
+        console.log(cart, cart.length)
+
+        
+        
     })
+
+
 })
+
+
+let updatedCart = localStorage.getItem("carts");
+
+cart = JSON.parse(updatedCart) || []
+console.log(cart)
+
+let cartHtml = ``
+
+cart.forEach ((cartItem)=>{
+    cartHtml += `
+    <div class="product-contain">
+                <div class="cart-image-container"><img src="${cartItem.image}"></div>
+                <div class="details">
+                    <p>${cartItem.name}</p>
+                    <p class="stock">In stock!!!</p>
+                    <div class="quantity">
+                    <label for="for">Qty</label>
+                    <select name="for">
+                        <option value="1">1</option>
+                        <option value="1">2</option>
+                        <option value="1">3</option>
+                        <option value="1">4</option>
+                        <option value="1">5</option>
+                        <option value="1">6</option>
+                        <option value="1">7</option>
+                        <option value="1">8</option>
+                        <option value="1">9</option>
+                        <option value="1">10</option>
+                    </select>
+                    </div>
+                    <button class="remove">Remove from cart</button>
+                </div>
+                <div><h4>${cartItem.price}</h4></div>
+            </div>
+    `
+})
+
+if(window.document.title === "Cart"){
+    document.querySelector(".container1").innerHTML = cartHtml
+}
+
+
+
